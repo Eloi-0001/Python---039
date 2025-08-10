ec3bd037010441252aedef3f12c6116bf9333508
José Lucas Eloi <lucaseloideolibeira@gmail.com>
2025-08-10 20:44:07 -0300

    Código 'pronto'
    Versão final do entregáveis. (sujeito a mudanças futuras)

--- Projeto 1/app.py
+++ Projeto 1/app.py
@@ -1,7 +1,27 @@
+# Fusão de divisoria.
+def D():
+    print('-'*30)
+
+# Função exibição.
+
+
+def L():
+    for liste in lista:
+        print(liste)
+
+# Função junta funções.
+
+
+def LD():
+    "Função que junta funções"
+    L()
+    D()
+
 
 print()
 print('Dados do primeiro usuario')
 print()
+
 # Criei um dicionario com registro de nome, email e telefone do usuario.
 Dados1 = {
     "Nome": "Joãozinho",
@@ -13,11 +33,7 @@ Dados1 = {
 for k, v in Dados1.items():
     print(f'O {k} é {v}!')
 
-# Fusão de divisoria.
-
-
-def D():
-    print('-'*30)
+D()
 
 
 # Gerei mais um usuario.
@@ -33,23 +49,12 @@ print(lista)
 
 D()
 
-# Função de organização.
-
-
-def L():
-    for liste in lista:
-        print(liste)
-
-
-L()
-
-D()
+LD()
 
 print('Vamos cadastrar um novo usuario!!')
 print()
 
 # (Metodo de inclusão de novos registros)
-
 # Gerei um novo dicionario vazio.
 Dados3 = {}
 
@@ -60,16 +65,15 @@ Dados3["Email"] = input("Me diga seu email!")
 Dados3["Telefone"] = input("Insira seu contato Celular!")
 
 # Adicionar os dados3 dentro da lista.
-lista.append(Dados3.copy())
+lista.append(Dados3)
 
 D()
 print('(Dados3) Adicionado com susseso!.')
 print()
 
 # Conferir se realmente os dados3 foram adicionados (!spoiler!, foram sim).
-L()
 
-D()
+LD()
 
 # Alteração no Dados1 (não dinamica).
 print('(Dados1) atualizados!')
@@ -79,9 +83,7 @@ Dados1["Nome"] = ("Nome generico")
 Dados1["Email"] = ("Email generico")
 Dados1["Telefone"] = ("Telefone generico")
 
-L()
-
-D()
+LD()
 
 # Atualizar Dados1 (com input).
 print('Alterar (Dados1)')
@@ -91,28 +93,20 @@ Dados1["Nome"] = input("Alterar nome")
 Dados1["Email"] = input("Alterar Email")
 Dados1["Telefone"] = input("Alterar Telefone")
 
-# Escolher qual dado deseja alterar.
-Alterar = input('Deseja alterar dados de quem?')
-
-
 D()
 
 print('(Dados1) atulaizados!')
 print()
 
-L()
-
-
-D()
+LD()
 
 # Adicionando IDS.
 print('IDS Adicionados com susseso!.')
 print()
 
-Dados1["ID"] = int(('01'))
-Dados2["ID"] = int(('02'))
-# NOTA ESSA LINHA NÂO ESTÀ FAZENDO NADA. CORRIGIR FUTURAMENTE
-Dados3["ID"] = int(('03'))
+Dados1["ID"] = '01'
+Dados2["ID"] = '02'
+Dados3["ID"] = '03'
 
 L()
 
@@ -121,33 +115,70 @@ print(type(Dados1['ID']))
 
 D()
 
-# Deletar os dados através do ID.
+# Menu interativo.
+while True:
+    Escolha = (input('Oque deseja fazer?. \n 01 = Editar um registro \n 02 = Deletar algum registro \n 03 = Adicionar um novo registro \n 04 = Sair.'))
+    if Escolha == '1':
+        Registro = input('Deseja editar o registro de quem?')
+        if Registro == '1':
+            Dados1["Nome"] = input("Alterar nome")
+            Dados1["Email"] = input("Alterar Email")
+            Dados1["Telefone"] = input("Alterar Telefone")
+            print('Dados1 foram atualizados.')
+        elif Registro == '2':
+            Dados2["Nome"] = input("Alterar nome")
+            Dados2["Email"] = input("Alterar Email")
+            Dados2["Telefone"] = input("Alterar Telefone")
+            print('Dados2 foram atualizados.')
+        elif Registro == '3':
+            Dados3["Nome"] = input("Alterar nome")
+            Dados3["Email"] = input("Alterar Email")
+            Dados3["Telefone"] = input("Alterar Telefone")
+            print('Dados3 foram atualizados.')
 
-Deletar = int(input("Você que deletar os dados de quem?"))
+    # Deletar os dados através do ID.
+    elif Escolha == '2':
+        Deletar = input(
+            "Você que deletar os dados de quem? (ex: 1 = Dados1, 2 = Dados2)")
 
-if Deletar == 2:
-    Dados2.pop('Email')
-    Dados2.pop('Nome')
-    Dados2.pop('Telefone')
-    Dados2.pop('ID')
+        if Deletar == '2':
+            Dados2.pop('Email')
+            Dados2.pop('Nome')
+            Dados2.pop('Telefone')
+            Dados2.pop('ID')
+            print('Dados2 removido.')
+        elif Deletar == '1':
+            Dados1.pop('Email')
+            Dados1.pop('Nome')
+            Dados1.pop('Telefone')
+            Dados1.pop('ID')
+            print('Dados1 removido.')
+        elif Deletar == '3':
+            Dados3.pop('Email')
+            Dados3.pop('Nome')
+            Dados3.pop('Telefone')
+            Dados3.pop('ID')
+            print('Dados3 removido.')
+        else:
+            print('Infelizmente, não identifeiquei esse registro.')
+    # Adicionar um novo registro.
+    elif Escolha == '3':
+        Dados4 = {}
+        Dados4["Nome"] = input("Digite o seu nome!")
+        Dados4["Email"] = input("Me diga seu email!")
+        Dados4["Telefone"] = input("Insira seu contato Celular!")
+        Dados4["ID"] = '04'
+        lista.append(Dados4)
+        print('Dados4 foi adicionado ao registro.')
+        LD()
+    # Não fazer nada.
+    elif Escolha == '4':
+        D()
+        print('Adeus!!')
+        D()
+        break
+    # Erro no codigo
+    else:
+        print('ISSO NÂO, BURRO, ESTUPIDO.')
 
-elif Deletar == 1:
-    Dados1.pop('Email')
-    Dados1.pop('Nome')
-    Dados1.pop('Telefone')
-    Dados1.pop('ID')
-
-elif Deletar == 3:
-    Dados3.pop('Email')
-    Dados3.pop('Nome')
-    Dados3.pop('Telefone')
-    Dados3.pop('ID')
-
-else:
-    print('Infelizmente, não identifeiquei esse registro.')
-
-D()
-
-for liste in lista:
-    if liste:
-        print(liste)
+    D()
