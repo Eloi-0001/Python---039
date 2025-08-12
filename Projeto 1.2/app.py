4e34561d4d3d4f5cb6a13ab957deae2dd09955da
José Lucas Eloi <lucaseloideolibeira@gmail.com>
2025-08-12 09:59:51 -0300

    Add files via upload

---
+++ Projeto 1.2/app.py
@@ -0,0 +1,5 @@
+import func 
+Lista_Registros = []
+func.Dados(Lista_Registros)
+print(Lista_Registros)
+input('Bom dia. \n 01 = Criar um registro \n 02 = Editar um registro \n 03 = Excluir um registro \n 04 = Sair')
---
+++ Projeto 1.2/func.py
@@ -0,0 +1,12 @@
+def Dados(lista:list):
+    "funão que gera dados de registro"
+    Registros = {}
+    Registros['Nome'] = input('Insira nome?')
+    Registros['Email'] = input('Insira email')
+    Registros['Telefone'] = input('Insira telefone')
+    lista.append(Registros)
+
+    
+    
+ 
+    
