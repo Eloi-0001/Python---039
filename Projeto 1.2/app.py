f0b16ee2eb033d853e670f7254eab8803f7b85db
José Lucas Eloi <lucaseloideolibeira@gmail.com>
2025-08-13 11:52:33 -0300

    Adicionando mais funções (deletar, editar, view).

--- Projeto 1.2/app.py
+++ Projeto 1.2/app.py
@@ -1,5 +1,9 @@
 import func 
-Lista_Registros = []
-func.Dados(Lista_Registros)
-print(Lista_Registros)
-input('Bom dia. \n 01 = Criar um registro \n 02 = Editar um registro \n 03 = Excluir um registro \n 04 = Sair')
+lista_registros = []
+func.registro_dados(lista_registros)
+print(lista_registros)
+func.editar_dados(lista_registros)
+print(lista_registros)
+func.deletar_dados(lista_registros)
+print(lista_registros)
+#input('Bom dia. \n 01 = Criar um registro \n 02 = Editar um registro \n 03 = Excluir um registro \n 04 = Sair')
--- Projeto 1.2/func.py
+++ Projeto 1.2/func.py
@@ -1,12 +1,27 @@
-def Dados(lista:list):
-    "funão que gera dados de registro"
-    Registros = {}
-    Registros['Nome'] = input('Insira nome?')
-    Registros['Email'] = input('Insira email')
-    Registros['Telefone'] = input('Insira telefone')
-    lista.append(Registros)
+"qq"
 
-    
-    
- 
-    
+def registro_dados(lista:list):
+    "funão que gera dados de registro"
+    registros = {}
+    registros['nome'] = input('Insira nome?')
+    registros['email'] = input('Insira email')
+    registros['telefone'] = input('Insira telefone')
+    lista.append(registros)
+
+def deletar_dados(lista:list):
+    "Função que deleta registros"
+    ind = int(input("Qual registro da lista você deseja remover?"))
+    lista.pop(ind)
+
+def editar_dados(lista:list):
+    "Função que edita registros"
+    index = int(input("Qual registro da lista você deseja editar?"))
+    registro = lista[index]
+    chave = input("Qual campo você deseja alterar?")
+    registro[chave] = input("Qual o novo valor?")
+
+def exibir_dados(lista:list):
+    "Apenas exibe a lista"
+
+    for registro in lista:
+        print(registro)
