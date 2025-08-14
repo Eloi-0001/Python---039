b442f36511e9de4a162c66c999c1df60548aa72f
José Lucas Eloi <lucaseloideolibeira@gmail.com>
2025-08-14 08:37:42 -0300

    atualização, qualidade de vida.
    adicionei "and" dentro do if e do else.

--- Projeto 1.2/app.py
+++ Projeto 1.2/app.py
@@ -1,20 +1,23 @@
 import func
 lista_registros = []
+
+
 while True:
     escolha = input(
-        'Bom dia. \n 01 = Criar um registro \n 02 = Editar um registro \n 03 = Excluir um registro \n 04 = Visualizar os registros \n 05 = Sair:')
-    if escolha == "1":
+        'Bom dia. \n 01 = Criar um registro. \n 02 = Editar um registro. \n 03 = Excluir um registro. \n 04 = Visualizar os registros. \n 05 = Sair.\n:')
+    if escolha == "1" and "01":
         func.registro_dados(lista_registros)
         print('Registro criado com sucesso!')
-    elif escolha == "2":
+    elif escolha == "2" and "02":
         func.editar_dados(lista_registros)
         print('Dados editados com sucesso!')
-    elif escolha == "3":
+    elif escolha == "3" and "03":
         func.deletar_dados(lista_registros)
         print('Dados deletados com sucesso!')
-    elif escolha == "4":
+    elif escolha == "4" and "04":
         for registros in lista_registros:
             print(registros)
     else:
         print('Adeus')
+
         break
