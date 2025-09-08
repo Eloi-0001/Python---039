5772bfd2e72d807556853b114537d5ada8dc60b4
José Lucas Eloi <lucaseloideolibeira@gmail.com>
2025-09-08 10:31:06 -0300

    surv e idade

--- Projeto 4/main.py
+++ Projeto 4/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv("Projeto 4/train.csv")
 
@@ -9,10 +10,16 @@ df['Age'] = df['Age'].fillna(df['Age'].mean())
 
 conversao = df['Age'].astype('int64')
 
-print(conversao)
+filtro_sobreviventes = df['Survived'] == 1
+sobreviventes = df[filtro_sobreviventes]
 
-df.info()
+f_morto = df['Survived'] == 0
+mortos = df[f_morto]
 
+plt.boxplot([mortos['Age'], sobreviventes['Age']], labels=['Mortos', 'Sobreviventes'])
+plt.title('Idade por sobrevivência')
+plt.ylabel('Idade')
+plt.show()
 
-
-
+sns.histplot(data=df, x='Age', hue='Survived', kde=True, bins=30, palette={0: 'red', 1: 'green'}, alpha=0.6)
+plt.show()
